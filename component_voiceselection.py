3c3e76a978671a82819dc361836c07589f09f37d
luc wastiaux <luc.wastiaux@airpost.net>
2021-12-07 10:38:29 +0800

    show audio language filters

--- component_voiceselection.py
+++ component_voiceselection.py
@@ -17,6 +17,22 @@ class VoiceSelection():
         # 4. draw all voices
 
         voice_list = self.hypertts.service_manager.full_voice_list()
+
+        languages = set()
+        audio_languages = set()
+        services = set()
+        genders = set()
+
+        for voice in voice_list:
+            audio_languages.add(voice.language)
+            languages.add(voice.language.lang)
+            services.add(voice.service.name)
+            genders.add(voice.gender)
+
+        audio_language_combobox = PyQt5.QtWidgets.QComboBox()
+        audio_language_combobox.addItems([audio_lang.audio_lang_name for audio_lang in audio_languages])
+        layout.addWidget(audio_language_combobox)
+
         for voice in voice_list:
             self.draw_voice(layout, voice)
 
