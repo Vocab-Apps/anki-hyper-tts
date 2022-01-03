a97499a22464c0f242a740a4bee320cb7f32b4ec
luc wastiaux <luc.wastiaux@airpost.net>
2022-01-03 15:45:34 +0800

    show preview records table

--- component_source.py
+++ component_source.py
@@ -2,11 +2,55 @@ import PyQt5
 
 import constants
 
+class SourceTextPreviewTableModel(PyQt5.QtCore.QAbstractTableModel):
+    def __init__(self):
+        PyQt5.QtCore.QAbstractTableModel.__init__(self, None)
+        self.source_records = []
+        self.note_id_header = 'Note Id'
+        self.source_text_header = 'Source Text'
+
+    def flags(self, index):
+        return PyQt5.QtCore.Qt.ItemIsSelectable | PyQt5.QtCore.Qt.ItemIsEnabled
+
+    def setSourceRecords(self, source_records):
+        self.source_records = source_records
+        start_index = self.createIndex(0, 0)
+        end_index = self.createIndex(len(self.source_records)-1, 0)
+        self.dataChanged.emit(start_index, end_index)        
+
+    def rowCount(self, parent):
+        return len(self.source_records)
+
+    def columnCount(self, parent):
+        return 2
+
+    def data(self, index, role):
+        if not index.isValid():
+            return PyQt5.QtCore.QVariant()
+        elif role != PyQt5.QtCore.Qt.DisplayRole:
+           return PyQt5.QtCore.QVariant()
+        if index.column() == 0:
+            # note_id
+            return PyQt5.QtCore.QVariant(self.self.source_records[index.row()][0])
+        else:
+            # source text field
+            return PyQt5.QtCore.QVariant(self.self.source_records[index.row()][1])
+
+    def headerData(self, col, orientation, role):
+        if orientation == PyQt5.QtCore.Qt.Horizontal and role == PyQt5.QtCore.Qt.DisplayRole:
+            if col == 0:
+                return PyQt5.QtCore.QVariant(self.note_id_header)
+            else:
+                return PyQt5.QtCore.QVariant(self.source_text_header)
+        return PyQt5.QtCore.QVariant()
+
 class BatchSource():
     def __init__(self, hypertts, note_id_list):
         self.hypertts = hypertts
         self.note_id_list = note_id_list
         self.field_list = self.hypertts.get_all_fields_from_notes(self.note_id_list)
+
+        self.source_text_preview_table_model = SourceTextPreviewTableModel()
 
     def draw(self, layout):
         self.batch_source_layout = PyQt5.QtWidgets.QVBoxLayout()
@@ -21,6 +65,13 @@ class BatchSource():
         self.source_field_combobox = PyQt5.QtWidgets.QComboBox()
         self.source_field_combobox.addItems(self.field_list)
         self.batch_source_layout.addWidget(self.source_field_combobox)
+
+        self.table_view = PyQt5.QtWidgets.QTableView()
+        self.table_view.setModel(self.source_text_preview_table_model)
+        header = self.table_view.horizontalHeader()
+        header.setSectionResizeMode(0, PyQt5.QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(1, PyQt5.QtWidgets.QHeaderView.Stretch)
+        self.batch_source_layout.addWidget(self.table_view)
 
         # wire events
         self.batch_mode_combobox.currentIndexChanged.connect(self.batch_mode_change)
@@ -41,4 +92,5 @@ class BatchSource():
     def source_field_change(self, current_index):
         field_name = self.field_list[current_index]
         field_values = self.hypertts.get_field_values_array(self.note_id_list, field_name)
-        print(field_values)
+        self.source_text_preview_table_model.setSourceRecords(field_values)
+        
