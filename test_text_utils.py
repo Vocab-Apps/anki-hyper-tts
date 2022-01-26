e98692c1e33e9d4b2c908a4e12852bc6b7676ecd
luc wastiaux <luc.wastiaux@airpost.net>
2022-01-26 10:26:47 +0800

    cleanup tests, ensure error handling is good

--- test_text_utils.py
+++ test_text_utils.py
@@ -4,94 +4,6 @@ import constants
 import unittest
 import config_models
 
-def test_is_empty(qtbot):
-    utils = text_utils.TextUtils({})
-
-    assert utils.is_empty('yo') == False
-    assert utils.is_empty('') == True
-    assert utils.is_empty(' ') == True
-    assert utils.is_empty('&nbsp;') == True
-    assert utils.is_empty('&nbsp; ') == True
-    assert utils.is_empty(' &nbsp; ') == True
-    assert utils.is_empty('<br>') == True
-    assert utils.is_empty('<div>\n</div>') == True
-
-def test_process(qtbot):
-    utils = text_utils.TextUtils({})
-
-    assert utils.process('<b>hello</b> world') == 'hello world'
-    assert utils.process('<span style="color: var(--field-fg); background: var(--field-bg);">&nbsp;gerund</span>') == 'gerund'
-
-def test_replace(qtbot):
-    utils = text_utils.TextUtils({'replacements': [
-        {'pattern': ' / ', 
-        'replace': ' '},
-        {'pattern': r'\(etw \+D\)', 
-        'replace': 'etwas +Dativ'},        
-    ]})
-
-    assert utils.process('word1 / word2') == 'word1 word2'
-    assert utils.process('<b>word1</b> / word2') == 'word1 word2'
-    assert utils.process('unter (etw +D)') == 'unter etwas +Dativ'
-    assert utils.process('<b>unter</b> (etw +D)') == 'unter etwas +Dativ'
-
-
-def test_replacement(qtbot):
-    text_replacement = text_utils.TextReplacement({
-        'pattern': ' / ', 
-        'replace': ' '
-    })
-    
-    assert text_replacement.process('word1 / word2') == 'word1 word2'
-
-    actual_dict_data = text_replacement.to_dict()
-    expected_dict_data = {
-        'pattern': ' / ', 
-        'replace': ' ',
-        'replace_type': 'regex'
-    }
-
-    assert actual_dict_data == expected_dict_data
-
-    # try to re-create from the dict
-    text_replacement_2 = text_utils.TextReplacement(actual_dict_data)
-    assert text_replacement_2.process('word1 / word2') == 'word1 word2'
-
-    # malformed input
-    text_replacement = text_utils.TextReplacement({
-        'pattern': ' / '
-    })
-    assert text_replacement.process('word1') == 'word1'
-
-def test_replacement_regexp_error(qtbot):
-    text_replacement = text_utils.TextReplacement({
-        'pattern': 'yoyo)', 
-        'replace': 'rep'
-    })
-
-    testcase_instance = unittest.TestCase()
-    testcase_instance.assertRaises(errors.TextReplacementError, text_replacement.process, 'yoyo')
-
-def test_replacement_simple(qtbot):
-    text_replacement = text_utils.TextReplacement({
-        'pattern': 'yoyo)', 
-        'replace': 'rep',
-        'replace_type': 'simple'
-    })
-    
-    assert text_replacement.process('yoyo') == 'yoyo'
-    assert text_replacement.process('yoyo)') == 'rep'
-
-    expected_dict = {
-        'pattern': 'yoyo)', 
-        'replace': 'rep',
-        'replace_type': 'simple'
-    }
-    assert text_replacement.to_dict() == expected_dict
-
-    text_replacement2 = text_utils.TextReplacement(expected_dict)
-    assert text_replacement2.process('yoyo)') == 'rep'
-
 def test_process_text(qtbot):
 
     # simple replacement
@@ -110,3 +22,17 @@ def test_process_text(qtbot):
     text_processing.add_text_replacement_rule(rule)
 
     assert text_utils.process_text('unter (etw +D)', text_processing) == 'unter etwas +Dativ'
+    assert text_utils.process_text('<b>unter</b> (etw +D)', text_processing) == 'unter etwas +Dativ'
+
+
+def test_replacement_regexp_error(qtbot):
+    
+    # regex replacement with error
+    text_processing = config_models.TextProcessing()
+    rule = config_models.TextReplacementRule(constants.TextReplacementRuleType.Regex)
+    rule.source = 'yoyo)'
+    rule.target = 'rep'
+    text_processing.add_text_replacement_rule(rule)
+
+    testcase_instance = unittest.TestCase()
+    testcase_instance.assertRaises(errors.TextReplacementError, text_utils.process_text, 'yoyo', text_processing)
--- text_utils.py
+++ text_utils.py
@@ -39,64 +39,12 @@ def process_text(source_text, text_processing_model):
     return processed_text
 
 def process_text_replacement_rule(input_text, rule):
-    if rule.rule_type == constants.TextReplacementRuleType.Regex:
-        result = re.sub(rule.source, rule.target, input_text)
-    elif rule.rule_type == constants.TextReplacementRuleType.Simple:
-        result = input_text.replace(rule.source,  rule.target)
-    return result
-
-def create_text_replacement():
-    return TextReplacement({
-        'pattern': None,
-        'replace': None,
-        'replace_type': constants.ReplaceType.simple.name
-    })
-
-
-class TextReplacement():
-    def __init__(self, options):
-        self.pattern = options.get('pattern', None)
-        self.replace = options.get('replace', None)
-        replace_type_str = options.get('replace_type', constants.ReplaceType.regex.name)
-        self.replace_type = constants.ReplaceType[replace_type_str]
-
-    def to_dict(self):
-        data = {
-            'pattern': self.pattern,
-            'replace': self.replace,
-            'replace_type': self.replace_type.name
-        }
-        return data
-
-    def process(self, text):
-        result = text
-        if self.pattern != None and self.replace != None:
-            try:
-                if self.replace_type == constants.ReplaceType.regex:
-                    result = re.sub(self.pattern, self.replace, text)
-                elif self.replace_type == constants.ReplaceType.simple:
-                    result = result.replace(self.pattern,  self.replace)
-                else:
-                    raise Exception(f'unsupported replacement type: {self.replace_type}')
-            except Exception as e:
-                raise errors.TextReplacementError(text, self.pattern, self.replace, str(e))
+    try:
+        if rule.rule_type == constants.TextReplacementRuleType.Regex:
+            result = re.sub(rule.source, rule.target, input_text)
+        elif rule.rule_type == constants.TextReplacementRuleType.Simple:
+            result = input_text.replace(rule.source,  rule.target)
         return result
+    except Exception as e:
+        raise errors.TextReplacementError(input_text, rule.source, rule.target, str(e))
 
-class TextUtils():
-    def __init__(self, options):
-        self.options = options
-        replacements_array = self.options.get('replacements', [])
-        self.replacements = [TextReplacement(replacement) for replacement in replacements_array]
-
-    def is_empty(self, text):
-        stripped_field_value = anki.utils.htmlToTextLine(text)
-        return len(stripped_field_value) == 0
-
-    def process(self, text):
-        result = anki.utils.htmlToTextLine(text)
-
-        # apply replacements
-        for replacement in self.replacements:
-            result = replacement.process(result)
-
-        return result
