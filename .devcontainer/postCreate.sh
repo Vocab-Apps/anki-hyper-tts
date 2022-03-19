e1abc3751170e3743cd808d7a0fd11bfb3023bf0
Luc <luc.wastiaux@xsmail.com>
2022-03-19 06:52:13 +0000

    github codespaces setup with vncserver/novnc

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# setup vncserver
+cp -rv $PWD/.devcontainer/config/.vnc $HOME/
+touch ~/.Xauthority
+rm -f /tmp/.X*-lock
+rm -f /tmp/.X11-unix/X*
+sudo ln -s $PWD/.devcontainer/services/vncserver /etc/init.d/vncserver
+sudo update-rc.d vncserver defaults 99
+sudo service vncserver start
+# setup novnc
+sudo ln -s $PWD/.devcontainer/config/novnc/index.html /opt/novnc/index.html
+sudo ln -s $PWD/.devcontainer/services/novnc /etc/init.d/novnc
+sudo update-rc.d novnc defaults 99
+sudo service novnc start
