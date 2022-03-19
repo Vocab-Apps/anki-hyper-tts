50671a760ba3a88e433fd8386fc195d800ecf7ac
Luc <luc.wastiaux@xsmail.com>
2022-03-19 08:44:35 +0000

    setup xvfb on startup

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -13,3 +13,7 @@ sudo ln -s $PWD/.devcontainer/config/novnc/index.html /opt/novnc/index.html
 sudo ln -s $PWD/.devcontainer/services/novnc /etc/init.d/novnc
 sudo update-rc.d novnc defaults 99
 sudo service novnc start
+# setup xvfb
+sudo ln -s $PWD/.devcontainer/services/xvfb /etc/init.d/xvfb
+sudo update-rc.d xvfb defaults 99
+sudo service xvfb start
