dd2ed16e8539ad6022221227db6a9ac5b502e92e
Luc <luc.wastiaux@xsmail.com>
2022-03-19 14:11:25 +0000

    add post start command

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,33 +4,6 @@
 mkdir -p $HOME/.local/share/Anki2/addons21
 ln -s /workspaces/anki-hyper-tts $HOME/.local/share/Anki2/addons21/anki-hyper-tts
 
-# start up the processes we need directly
-
-# clear all locks on X servers
-sudo rm -f /tmp/.X*-lock
-sudo rm -f /tmp/.X11-unix/X*
-touch $HOME/.Xauthority
-
-# novnc
+# VNC setup
 sudo ln -s $PWD/.devcontainer/config/novnc/index.html /opt/novnc/index.html
-PIDFILE=/var/run/novnc.pid
-DAEMON=/opt/novnc/utils/novnc_proxy
-DAEMON_OPTS="--vnc localhost:5901"
-start-stop-daemon --start --quiet --background --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
-
-# xvfb
-PIDFILE=/var/run/xvfb.pid
-DAEMON=/usr/bin/Xvfb
-# will run on display 2
-DAEMON_OPTS=":2 -screen 0 1024x768x24 -ac +extension GLX +render -noreset"
-start-stop-daemon --start --quiet --background --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
-
-# vncserver on display :1
 cp -rv $PWD/.devcontainer/config/.vnc $HOME/
-
-# try running with start-stop-daemon
-PIDFILE=/var/run/vncserver.pid
-DAEMON=/usr/bin/vncserver
-# will run on display 2
-DAEMON_OPTS="-fg -SecurityTypes None -geometry 1920x1080 :1"
-start-stop-daemon --start --quiet --background --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
