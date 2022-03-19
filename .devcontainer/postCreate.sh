9deec9615c8db7561280e4d63f7b232731b72843
Luc <luc.wastiaux@xsmail.com>
2022-03-19 11:37:47 +0000

    symbolic link for addon

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# link workspace in anki addons
+mkdir -p $HOME/.local/share/Anki2/addons21
+ln -s /workspaces/anki-hyper-tts $HOME/.local/share/Anki2/addons21/anki-hyper-tts
 # setup vncserver
 cp -rv $PWD/.devcontainer/config/.vnc $HOME/
 touch ~/.Xauthority
