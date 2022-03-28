2f79615ea392dfff9b28f09283ddddda99e003dd
luc-languagetools <languagetools@mailc.net>
2022-03-28 10:12:45 +0800

    check sentry sdk version upfront

--- __init__.py
+++ __init__.py
@@ -30,7 +30,11 @@ else:
     # ============================================================
     from . import constants
     if constants.ENABLE_SENTRY_CRASH_REPORTING:
-        sys._sentry_crash_reporting = True
+        import sentry_sdk        
+        # check version. some anki addons package an obsolete version of sentry_sdk
+        sentry_sdk_int_version = int(sentry_sdk.VERSION.replace('.', ''))
+        if sentry_sdk_int_version >= 155:        
+            sys._sentry_crash_reporting = True
 
     # setup logger
     # ============
@@ -53,64 +57,60 @@ else:
     # ============================
 
     if hasattr(sys, '_sentry_crash_reporting'):
-        import sentry_sdk
-        # check version. some anki addons package an obsolete version of sentry_sdk
-        sentry_sdk_int_version = int(sentry_sdk.VERSION.replace('.', ''))
-        if sentry_sdk_int_version >= 155:
-            # setup crash reporting
-            # =====================
+        # setup crash reporting
+        # =====================
 
-            from . import version
+        from . import version
 
-            addon_config = aqt.mw.addonManager.getConfig(__name__)
-            api_key = addon_config.get('configuration', {}).get('hypertts_pro_api_key', None)
-            if api_key != None:
-                user_id = f'api_key:{api_key}'
-            else:
-                unique_id = addon_config.get('unique_id', None)
-                if unique_id == None:
-                    unique_id = f'uuid:{uuid.uuid4().hex[:12]}'
-                    addon_config['unique_id'] = unique_id
-                    aqt.mw.addonManager.writeConfig(__name__, addon_config)
-                user_id = unique_id
-
-            def sentry_filter(event, hint):
-                if 'exc_info' in hint:
-                    exc_type, exc_value, tb = hint['exc_info']
-
-                    event['contexts']['cloudlanguagetools'] = {'user_id': api_key}
-
-                    # do we recognize the paths in this stack trace ?
-                    relevant_exception = False
-                    stack_summary = traceback.extract_tb(tb)
-                    for stack_frame in stack_summary:
-                        filename = stack_frame.filename
-                        if 'anki-hyper-tts' in filename or '111623432' in filename:
-                            relevant_exception = True
-                    
-                    # if not, discard
-                    if not relevant_exception:
-                        return None
-
-                return event
-
-            traces_sample_rate_map = {
-                'development': 1.0,
-                'production': 0.175
-            }
-
-            # need to create an anki-hyper-tts project in sentry.io first
-            sentry_env = os.environ.get('SENTRY_ENV', 'production')
-            sentry_sdk.init(
-                "https://a4170596966d47bb9f8fda74a9370bc7@o968582.ingest.sentry.io/6170140",
-                traces_sample_rate=traces_sample_rate_map[sentry_env],
-                release=f'anki-hyper-tts@{version.ANKI_HYPER_TTS_VERSION}-{anki.version}',
-                environment=sentry_env,
-                before_send=sentry_filter
-            )
-            sentry_sdk.set_user({"id": user_id})
+        addon_config = aqt.mw.addonManager.getConfig(__name__)
+        api_key = addon_config.get('configuration', {}).get('hypertts_pro_api_key', None)
+        if api_key != None:
+            user_id = f'api_key:{api_key}'
         else:
-            logger.info(f'sentry_sdk.VERSION: {sentry_sdk.VERSION}, disabling crash reporting')
+            unique_id = addon_config.get('unique_id', None)
+            if unique_id == None:
+                unique_id = f'uuid:{uuid.uuid4().hex[:12]}'
+                addon_config['unique_id'] = unique_id
+                aqt.mw.addonManager.writeConfig(__name__, addon_config)
+            user_id = unique_id
+
+        def sentry_filter(event, hint):
+            if 'exc_info' in hint:
+                exc_type, exc_value, tb = hint['exc_info']
+
+                event['contexts']['cloudlanguagetools'] = {'user_id': api_key}
+
+                # do we recognize the paths in this stack trace ?
+                relevant_exception = False
+                stack_summary = traceback.extract_tb(tb)
+                for stack_frame in stack_summary:
+                    filename = stack_frame.filename
+                    if 'anki-hyper-tts' in filename or '111623432' in filename:
+                        relevant_exception = True
+                
+                # if not, discard
+                if not relevant_exception:
+                    return None
+
+            return event
+
+        traces_sample_rate_map = {
+            'development': 1.0,
+            'production': 0.175
+        }
+
+        # need to create an anki-hyper-tts project in sentry.io first
+        sentry_env = os.environ.get('SENTRY_ENV', 'production')
+        sentry_sdk.init(
+            "https://a4170596966d47bb9f8fda74a9370bc7@o968582.ingest.sentry.io/6170140",
+            traces_sample_rate=traces_sample_rate_map[sentry_env],
+            release=f'anki-hyper-tts@{version.ANKI_HYPER_TTS_VERSION}-{anki.version}',
+            environment=sentry_env,
+            before_send=sentry_filter
+        )
+        sentry_sdk.set_user({"id": user_id})
+    else:
+        logger.info(f'sentry_sdk.VERSION: {sentry_sdk.VERSION}, disabling crash reporting')
 
     # addon imports
     # =============
