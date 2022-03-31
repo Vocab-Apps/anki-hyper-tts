b19792e2d189029e3b479daca1a7a5e6ee8a0d1f
luc wastiaux <luc.wastiaux@airpost.net>
2022-03-31 22:08:00 +0800

    copy build settings from anki_new_format_pack

--- web/hypertts.js
+++
@@ -1,9 +0,0 @@
-// build using:
-// npm run build
-
-import HyperTTS, {configureEditorHyperTTS} from "./HyperTTS.svelte";
-
-$editorToolbar.then((editorToolbar) => {
-    console.log(configureEditorHyperTTS);
-    editorToolbar.toolbar.insertGroup({component: HyperTTS, id: "hypertts"});
-});
