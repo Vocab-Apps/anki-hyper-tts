2bbf38f84e93549a5b8818a236a56b50160321a2
luc wastiaux <luc.wastiaux@airpost.net>
2022-03-31 22:50:17 +0800

    make function available in window.

--- web/hypertts.ts
+++ web/hypertts.ts
@@ -4,7 +4,10 @@
 import * as NoteEditor from "anki/NoteEditor";
 import HyperTTS, {configureEditorHyperTTS} from "./HyperTTS.svelte";
 
+
 NoteEditor.lifecycle.onMount(({ toolbar }) => {
     console.log(configureEditorHyperTTS);
     toolbar.toolbar.append({component: HyperTTS, id: "hypertts"});
 });
+
+window.configureEditorHyperTTS = configureEditorHyperTTS;
