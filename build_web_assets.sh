267fb0ac2104d281e6af40ac18e10e0a158e2cc4
Luc W <lucw@airpost.net>
2023-01-12 18:16:28 +0800

    working js build

--- build_web_assets.sh
+++ build_web_assets.sh
@@ -3,4 +3,4 @@
 docker run -it --rm \
 --mount type=bind,source="$(pwd)"/web,target=/workspace/web \
 --mount type=bind,source="$(pwd)",target=/workspace/web_build_output \
-lucwastiaux/anki-addon-svelte-build:latest sh -c "cp -rv /workspace/web/* /workspace/web_build/ && cd /workspace/web_build/ && yarn build && cp /workspace/web_build/hypertts* /workspace/web_build_output"
+lucwastiaux/anki-addon-svelte-build:latest sh -c "cp -rv /workspace/web/* /workspace/web_build/ && cd /workspace/web_build/ && yarn build && cp /workspace/web_build/hypertts.css /workspace/web_build/hypertts.js /workspace/web_build_output"
