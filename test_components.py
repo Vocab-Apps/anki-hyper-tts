1cef0f1186c66d6722dc3d3810acd2e5eee691e3
Luc W <lucw@airpost.net>
2023-09-22 10:04:00 +0800

    test editor with loading of new preset

--- test_components.py
+++ test_components.py
@@ -1147,6 +1147,7 @@ def test_batch_dialog_editor(qtbot):
     batch = component_batch.create_component_batch_editor_new_preset(
         hypertts_instance, dialog, note, mock_editor, False, 'preset 1')
 
+    # select second voice
     batch.voice_selection.voices_combobox.setCurrentIndex(1)
 
     # test sound preview
@@ -1169,6 +1170,81 @@ def test_batch_dialog_editor(qtbot):
     # test apply to note
     # ==================
     # dialog.exec()
+    
+    # set target field
+    batch.target.target_field_combobox.setCurrentText('Sound')
+
+    # apply not note
+    qtbot.mouseClick(batch.apply_button, aqt.qt.Qt.MouseButton.LeftButton)
+
+    sound_tag = note.set_values['Sound']
+    audio_full_path = hypertts_instance.anki_utils.extract_sound_tag_audio_full_path(sound_tag)
+    audio_data = hypertts_instance.anki_utils.extract_mock_tts_audio(audio_full_path)
+
+    assert audio_data['source_text'] == '老人家'
+
+    assert mock_editor.set_note_called == True
+
+    assert hypertts_instance.anki_utils.undo_started == True
+    assert hypertts_instance.anki_utils.undo_finished == True
+
+    assert dialog.closed == True
+
+def test_batch_dialog_editor_create_then_load(qtbot):
+    config_gen = testing_utils.TestConfigGenerator()
+    hypertts_instance = config_gen.build_hypertts_instance_test_servicemanager('default')
+
+    note_id_list = [config_gen.note_id_1, config_gen.note_id_2]    
+    note = hypertts_instance.anki_utils.get_note_by_id(config_gen.note_id_1)
+
+    mock_editor = testing_utils.MockEditor()
+
+    dialog = build_empty_dialog()
+    batch = component_batch.create_component_batch_editor_new_preset(
+        hypertts_instance, dialog, note, mock_editor, False, 'preset 1')
+
+    # select second voice
+    batch.voice_selection.voices_combobox.setCurrentIndex(1)
+
+    # set preset name and save
+    # set profile name
+    preset_name = 'editor preset 1'
+    hypertts_instance.anki_utils.ask_user_get_text_response = preset_name
+    qtbot.mouseClick(batch.profile_rename_button, aqt.qt.Qt.MouseButton.LeftButton)
+    # save
+    qtbot.mouseClick(batch.profile_save_button, aqt.qt.Qt.MouseButton.LeftButton)
+
+    # close the dialog
+    dialog.close()
+
+    # make sure the preset was saved
+    preset_uuid = 'uuid_1'
+    assert preset_uuid in hypertts_instance.anki_utils.written_config[constants.CONFIG_PRESETS]
+
+    # now, open the dialog, with the existing preset
+    dialog = build_empty_dialog()
+    batch = component_batch.create_component_batch_editor_existing_preset(
+        hypertts_instance, dialog, note, mock_editor, False, preset_uuid)
+
+    # test sound preview
+    # ==================
+    # dialog.exec()
+    qtbot.mouseClick(batch.preview_sound_button, aqt.qt.Qt.MouseButton.LeftButton)
+    assert hypertts_instance.anki_utils.played_sound == {
+        'source_text': '老人家',
+        'voice': {
+            'gender': 'Male', 
+            'language': 'fr_FR', 
+            'name': 'voice_a_1', 
+            'service': 'ServiceA',
+            'voice_key': {'name': 'voice_1'}
+        },
+        'options': {}
+    }    
+
+
+    # test apply to note
+    # ==================
     
     # set target field
     batch.target.target_field_combobox.setCurrentText('Sound')
