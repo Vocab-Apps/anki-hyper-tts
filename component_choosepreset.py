0b1a640c55d794b3485d08374b331313fc584952
Luc W <lucw@airpost.net>
2023-10-01 15:43:51 +0800

    add a test with existing presets

--- component_choosepreset.py
+++ component_choosepreset.py
@@ -1,7 +1,10 @@
 import sys
 import aqt.qt
 
+from typing import List, Optional
+
 component_common = __import__('component_common', globals(), locals(), [], sys._addon_import_level_base)
+config_models = __import__('config_models', globals(), locals(), [], sys._addon_import_level_base)
 
 
 class ComponentChoosePreset(component_common.ComponentBase):
@@ -23,4 +26,30 @@ class ComponentChoosePreset(component_common.ComponentBase):
         self.vlayout.addWidget(self.new_preset_radio_button)
         self.vlayout.addWidget(self.existing_preset_radio_button)
 
+        self.preset_combo_box = aqt.qt.QComboBox()
+        # get preset list
+        self.preset_list: List[config_models.PresetInfo] = self.hypertts.get_preset_list()
+        for preset_info in self.preset_list:
+            self.preset_combo_box.addItem(preset_info.name, preset_info)
+        self.vlayout.addWidget(self.preset_combo_box)
+        
+        # only enable "existing preset" if we have at least one preset
+        existing_presets_available = len(self.preset_list) > 0
+        self.existing_preset_radio_button.setEnabled(existing_presets_available)
+        self.preset_combo_box.setEnabled(existing_presets_available)
+
+        # wire events
+        self.new_preset_radio_button.toggled.connect(self.new_preset_radio_button_checked)
+        self.existing_preset_radio_button.toggled.connect(self.existing_preset_radio_button_checked)
+
         layout.addLayout(self.vlayout)
+
+    def update_controls_state(self):
+        self.new_preset = self.new_preset_radio_button.isChecked()
+        self.preset_combo_box.setEnabled(not self.new_preset)
+
+    def new_preset_radio_button_checked(self):
+        self.update_controls_state()
+
+    def existing_preset_radio_button_checked(self):
+        self.update_controls_state()
--- test_component_choosepreset.py
+++ test_component_choosepreset.py
@@ -14,7 +14,7 @@ import component_choosepreset
 
 logger = logging.getLogger(__name__)
 
-def test_choose_preset_1(qtbot):
+def test_choose_preset_no_presets(qtbot):
     config_gen = testing_utils.TestConfigGenerator()
     hypertts_instance = config_gen.build_hypertts_instance_test_servicemanager('default')
     dialog = gui_testing_utils.build_empty_dialog()
@@ -24,4 +24,28 @@ def test_choose_preset_1(qtbot):
     # check the defaults
     assert choosepreset.new_preset_radio_button.isChecked()
     assert choosepreset.new_preset == True
+    # no presets are available, so the "existing preset" radio button is disabled
+    assert choosepreset.existing_preset_radio_button.isEnabled() == False
+    assert choosepreset.preset_combo_box.isEnabled() == False
+
+
+def test_choose_preset_existing_presets(qtbot):
+    config_gen = testing_utils.TestConfigGenerator()
+    hypertts_instance = config_gen.build_hypertts_instance_test_servicemanager('default')
+    dialog = gui_testing_utils.build_empty_dialog()
+
+    # create some dummy presets
+    hypertts_instance.anki_utils.config[constants.CONFIG_PRESETS] = {
+        'uuid_0': {'name': 'my preset 4'},
+        'uuid_1': {'name': 'my preset 5'}
+    }
+
+    choosepreset = component_choosepreset.ComponentChoosePreset(hypertts_instance, dialog)
+    choosepreset.draw(dialog.layout())
+    # check the defaults
+    assert choosepreset.new_preset_radio_button.isChecked()
+    assert choosepreset.new_preset == True
+    # presets are available, so the other controls should be enabled
+    assert choosepreset.existing_preset_radio_button.isEnabled() == True
+    assert choosepreset.preset_combo_box.isEnabled() == True
 
