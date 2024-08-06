242ba288c8944ffdd4c76a83915f5b32e312be28
Luc W <lucw@airpost.net>
2024-08-07 06:55:03 +0800

    fixed cereproc

--- test_tts_services.py
+++ test_tts_services.py
@@ -382,8 +382,7 @@ class TTSTests(unittest.TestCase):
         assert len(service_voices) > 50
 
         # pick a random en_US voice
-        selected_voice = self.pick_random_voice(voice_list, service_name, languages.AudioLanguage.en_US)
-        self.verify_audio_output(selected_voice, 'This is the first sentence')
+        self.random_voice_test(service_name, AudioLanguage.en_US, 'This is the first sentence')
 
     def test_cereproc(self):
         # pytest test_tts_services.py  -k 'TTSTests and test_cereproc'
@@ -394,8 +393,7 @@ class TTSTests(unittest.TestCase):
         assert len(service_voices) > 50
 
         # pick a random en_US voice
-        selected_voice = self.pick_random_voice(voice_list, service_name, languages.AudioLanguage.en_GB)
-        self.verify_audio_output(selected_voice, 'This is the first sentence')
+        self.random_voice_test(service_name, AudioLanguage.en_GB, 'This is the first sentence')
 
     def test_elevenlabs_english(self):
         # pytest test_tts_services.py  -k 'TTSTests and test_elevenlabs_english'
