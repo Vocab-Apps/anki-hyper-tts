3cfe813e0ddbd59b0f00f38b6536cc65bdd18c2c
Luc W <lucw@airpost.net>
2024-09-19 21:47:29 +0800

    exclude more files

--- package.sh
+++ package.sh
@@ -33,9 +33,10 @@ rm -f meta.json
 rm -rf __pycache__
 rm -f user_files/*.mp3
 rm -f user_files/*.ogg
+rm -f *.mp3 *.ogg *.wav
 rm -rvf htmlcov/
 ADDON_FILENAME=${HOME}/anki-addons-releases/anki-hyper-tts-${VERSION_NUMBER}.ankiaddon
-zip --exclude "*node_modules*" "*__pycache__*" "test_*.py" "*test_services*" "*.ini" "*.workspace" "*.md" "*.sh" requirements.txt "*.code-workspace" "web" -r ${ADDON_FILENAME} *
+zip --exclude "*node_modules*" "*__pycache__*" "test_*.py" "*test_services*" "test_utils" "*.ini" "*.workspace" "*.md" "*.sh" requirements.txt "*.code-workspace" "web" -r ${ADDON_FILENAME} *
 
 # sync 
 rclone sync ~/anki-addons-releases/ dropbox:Anki/anki-addons-releases/
