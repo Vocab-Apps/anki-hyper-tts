f944cd048435b285db993c59e4a85f08392fe126
Luc W <lucw@airpost.net>
2024-09-21 16:51:58 +0800

    remove unneeded script

--- build_web_assets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker run -it --rm \
---mount type=bind,source="$(pwd)"/web,target=/workspace/web \
---mount type=bind,source="$(pwd)",target=/workspace/web_build_output \
-lucwastiaux/anki-addon-svelte-build:latest sh -c "cp -rv /workspace/web/* /workspace/web_build/ && cd /workspace/web_build/ && yarn build && cp /workspace/web_build/hypertts.css /workspace/web_build/hypertts.js /workspace/web_build_output"  || { echo 'docker for lucwastiaux/anki-addon-svelte-build failed' ; exit 1; }
