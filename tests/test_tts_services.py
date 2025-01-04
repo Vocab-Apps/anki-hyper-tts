95026c1db1d66c4bba1d45c8cd9e226daae7c178
Luc W <lucw@airpost.net>
2025-01-04 19:53:48 +0800

    add portuguese-portugal voice

--- tests/test_tts_services.py
+++ tests/test_tts_services.py
@@ -574,6 +574,34 @@ class TTSTests(unittest.TestCase):
         self.random_voice_test(service_name, AudioLanguage.en_US, 'Camera')
         self.random_voice_test(service_name, AudioLanguage.fr_FR, 'ordinateur')
 
+    def test_forvo_portuguese(self):
+        # pytest tests/test_tts_services.py  -k 'TTSTests and test_forvo_portuguese'
+        service_name = 'Forvo'
+        source_text = 'pomos'
+
+        voice_list = self.manager.full_voice_list()
+
+        # https://forvo.com/word/pomos/
+        # as of 2025/01/04:
+        # two recordings from brazil
+        # no recordings from portugal
+
+        # locate forvo portuguese-portugal voice
+        candidates = [voice for voice in voice_list 
+                      if voice.service == service_name and 
+                      AudioLanguage.pt_PT in voice.audio_languages and
+                      voice.gender == constants.Gender.Any]
+        assert len(candidates) == 1
+        forvo_portuguese_portugal_voice = candidates[0]
+
+        self.assertRaises(errors.AudioNotFoundError, self.verify_audio_output, forvo_portuguese_portugal_voice, AudioLanguage.pt_PT, source_text)
+        return
+
+        # self.verify_audio_output(forvo_portuguese_voice, AudioLanguage.pt_PT, source_text)
+
+        self.assertRaises(errors.AudioNotFoundError, self.random_voice_test, service_name, AudioLanguage.pt_PT, 'pomos')
+        self.random_voice_test(service_name, AudioLanguage.pt_BR, 'pomos')
+        # self.random_voice_test(service_name, AudioLanguage.fr_FR, 'ordinateur')
 
 
     def test_googletranslate(self):
