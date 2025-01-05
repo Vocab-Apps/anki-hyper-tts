4a51ab308c6ffd838e442522394a5958e2ce1a96
Luc W <lucw@airpost.net>
2025-01-05 08:03:03 +0800

    feat: Add component for easy voice selection in HyperTTS addon

---
+++ hypertts_addon/component_voiceselection_easy.py
@@ -0,0 +1,11 @@
+
+
+# VoiceSelectionEasy is going to be used with ComponentEasy to provide simplified access to the
+# voice list. It should inherit from VoiceSelection.
+# it should only show the following filter combo boxes:
+# - Language
+# - Service
+# it should also show the voice list in a combo box
+# since it only allows selecting one voice, the mode should always default to single voice.
+# no need to show the voice list (only required for random or priority modes)
+# the VoiceSelectionEasy component doesn't need a scroll area since it will be simpler and more compact.
