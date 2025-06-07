dabe3f5b1c24e338bfbfb6f3ebd0bd15ad58f520
Luc W <lucw@airpost.net>
2025-06-07 14:28:19 +0800

    simplify styles

--- hypertts_addon/component_services_configuration.py
+++ hypertts_addon/component_services_configuration.py
@@ -79,12 +79,12 @@ class ServicesConfigurationDialog(aqt.qt.QDialog):
         """
         
         trial_html = f"""
-        <div style="padding: 15px; border-radius: 5px 5px 0px 0px;">
+        <div>
             <h3 style="{h3_style} font-weight: bold; text-shadow: 1px 1px 2px rgba(0,0,0,0.3);">{constants.GUI_TEXT_SERVICES_CONFIG_TRIAL_TITLE}</h3>
         </div>
-        <div style="padding: 15px; border-radius: 0px 0px 5px 5px;">
-            <p style="margin: 0 0 10px 0;">{constants.GUI_TEXT_SERVICES_CONFIG_TRIAL_DESCRIPTION}</p>
-            <p style="margin: 0; font-style: italic;">{constants.GUI_TEXT_SERVICES_CONFIG_TRIAL_RECOMMENDED}</p>
+        <div>
+            <p>{constants.GUI_TEXT_SERVICES_CONFIG_TRIAL_DESCRIPTION}</p>
+            <p>{constants.GUI_TEXT_SERVICES_CONFIG_TRIAL_RECOMMENDED}</p>
         </div>
         """
         
@@ -107,12 +107,12 @@ class ServicesConfigurationDialog(aqt.qt.QDialog):
         """
         
         free_html = f"""
-        <div style="padding: 15px; border-radius: 5px 5px 0px 0px;">
+        <div>
             <h3 style="{h3_style}">{constants.GUI_TEXT_SERVICES_CONFIG_FREE_TITLE}</h3>
         </div>
-        <div style="padding: 15px; border-radius: 0px 0px 5px 5px;">
-            <p style="margin: 0 0 10px 0;">{constants.GUI_TEXT_SERVICES_CONFIG_FREE_DESCRIPTION}</p>
-            <p style="margin: 0; font-style: italic;">{constants.GUI_TEXT_SERVICES_CONFIG_FREE_RECOMMENDED}</p>
+        <div>
+            <p>{constants.GUI_TEXT_SERVICES_CONFIG_FREE_DESCRIPTION}</p>
+            <p>{constants.GUI_TEXT_SERVICES_CONFIG_FREE_RECOMMENDED}</p>
         </div>
         """
         
@@ -135,12 +135,12 @@ class ServicesConfigurationDialog(aqt.qt.QDialog):
         """
         
         manual_html = f"""
-        <div style="padding: 15px; border-radius: 5px 5px 0px 0px;">
+        <div>
             <h3 style="{h3_style}">{constants.GUI_TEXT_SERVICES_CONFIG_MANUAL_TITLE}</h3>
         </div>
-        <div style="padding: 15px; border-radius: 0px 0px 5px 5px;">
-            <p style="margin: 0 0 10px 0;">{constants.GUI_TEXT_SERVICES_CONFIG_MANUAL_DESCRIPTION}</p>
-            <p style="margin: 0; font-style: italic;">{constants.GUI_TEXT_SERVICES_CONFIG_MANUAL_RECOMMENDED}</p>
+        <div>
+            <p>{constants.GUI_TEXT_SERVICES_CONFIG_MANUAL_DESCRIPTION}</p>
+            <p>{constants.GUI_TEXT_SERVICES_CONFIG_MANUAL_RECOMMENDED}</p>
         </div>
         """
         
